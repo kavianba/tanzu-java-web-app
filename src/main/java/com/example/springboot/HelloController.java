5cb59aa62f3e812420569295e7725be978184693
Kavitha Anbazhagan <kavithaa@vmware.com>
2023-08-07 10:27:05 -0700

    Delivery stage testing

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu! - testing delivery Atempt 3";
+		return "Greetings from Spring Boot + Tanzu! - testing delivery Atempt 4";
 	}
 
 }
